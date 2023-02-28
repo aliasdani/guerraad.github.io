cbb26e47e22fcf8fcd4bc39cf9c065920812abd4
guerrita <dany_begins@hotmail.com.ar>
2023-02-28 17:26:28 -0300

    primer commit

---
+++ js/function.js
@@ -0,0 +1,18 @@
+function cambia() {
+    Element=document.getElementById("linkedin");
+    Element.src="img/linkedinlogo2.png";
+}
+function normal() {
+    Element=document.getElementById("linkedin");
+    Element.src="img/linkedinlogo.png";
+}
+
+
+function cambia1() {
+    Element=document.getElementById("github");
+    Element.src="img/githublogo2.png";
+}
+function normal1() {
+    Element =document.getElementById("github");
+    Element.src="img/githublogo.png";
+}
